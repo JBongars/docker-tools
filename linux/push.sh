676466caec841eaed4244a89f1c7b935a3276244
julien bongars <julienbongars23@gmail.com>
2023-03-06 05:00:20 +0800

    chore: remove powershell and bash scripts

--- linux/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-repo_name=$(npm view "$(dirname "$(dirname "$(realpath "$0")")")" docker_repository)
-images=$(docker images --filter "reference=${repo_name}/dtools_*" --format "{{.Repository}}:{{.Tag}}")
-
-echo "$images" | parallel -j0 -I{} sh -c 'echo {} is pushing... && docker push {} > /dev/null && echo -en "\r{} done"'
