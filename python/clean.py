c3e172e0de080e8427ff1fc3c2f6293c30a8d618
julien bongars <julienbongars23@gmail.com>
2023-03-13 00:36:52 +0800

    chore: remove log clean py

--- python/clean.py
+++ python/clean.py
@@ -4,9 +4,6 @@ import subprocess
 
 def remove_all_images():
     find_command = "docker images --filter \"reference=*/dtools_*\" --format \"{{.Repository}}:{{.Tag}}\""
-    images = subprocess.run(find_command).stdout
-
-    print("deleting images: ", images)
     remove_command = f"docker rmi $({find_command})"
 
     err = subprocess.run(remove_command).stderr
