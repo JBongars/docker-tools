5b93ccb3e395136cbee80d10c96688d124e00b2c
julien bongars <julienbongars23@gmail.com>
2023-04-07 14:35:54 +0800

    chore: refactor python scripts

--- python/build.py
+++ python/build.py
@@ -5,6 +5,7 @@ import json
 import sys
 import jinja2
 
+
 def get_template_header(image_name="<template>"):
     return f'''
 # THIS IS A GENERATED DOCKERFILE.
@@ -14,6 +15,7 @@ def get_template_header(image_name="<template>"):
 
 '''
 
+
 def get_package_json():
     script_path = os.path.dirname(os.path.realpath(__file__))
     package_json_path = os.path.join(script_path, "..", "package.json")
@@ -21,21 +23,26 @@ def get_package_json():
     with open(package_json_path) as f:
         return json.load(f)
 
+
 def get_project_version_no():
     return get_package_json()["version"]
+
 
 def get_project_path():
     script_path = os.path.dirname(os.path.realpath(__file__))
     return os.path.abspath(os.path.join(script_path, ".."))
 
+
 def get_repo_name():
     return get_package_json()["docker_repository"]
+
 
 def get_jinja2_env():
     script_path = os.path.dirname(os.path.realpath(__file__))
     jinja_path = os.path.join(script_path, '..', f"templates")
-    
+
     return jinja2.Environment(loader=jinja2.FileSystemLoader(jinja_path))
+
 
 def generate_docker_image(dockerfile_path, image_name):
     env = get_jinja2_env()
@@ -50,10 +57,17 @@ def generate_docker_image(dockerfile_path, image_name):
 
 
 def build_docker_image(dockerfile_path, local_tag_name, build_args):
-    subprocess.run(f"docker build -t {local_tag_name} {build_args} {dockerfile_path}", shell=True, check=True)
+    subprocess.run(
+        f"docker build -t {local_tag_name} {build_args} {dockerfile_path}",
+        shell=True,
+        check=True)
+
 
 def tag_docker_image(current_tag, next_tag):
-    subprocess.run(f"docker tag {current_tag} {next_tag}", shell=True, check=True)
+    subprocess.run(f"docker tag {current_tag} {next_tag}",
+                   shell=True,
+                   check=True)
+
 
 def process_docker_image(image_name, version, repo_name, build_args):
     print("build_args= ", build_args)
@@ -67,22 +81,27 @@ def process_docker_image(image_name, version, repo_name, build_args):
     local_tag_name = f"dtools_{image_name}:latest"
     tag_name = f"{repo_name}/dtools_{image_name}:{version}"
     latest_tag_name = f"{repo_name}/dtools_{image_name}:latest"
-    
+
     generate_docker_image(dockerfile_path, image_name)
     build_docker_image(dockerfile_path, local_tag_name, build_args)
     tag_docker_image(local_tag_name, tag_name)
     tag_docker_image(local_tag_name, latest_tag_name)
+
 
 def build_all_images(build_args):
     repo_name = get_repo_name()
     version = get_project_version_no()
     extension = ".dockerfile.j2"
 
-    image_paths = glob.glob(os.path.join(get_project_path(), f"templates/*{extension}"))
-    images = [os.path.basename(elem).replace(extension, "") for elem in image_paths]
+    image_paths = glob.glob(
+        os.path.join(get_project_path(), f"templates/*{extension}"))
+    images = [
+        os.path.basename(elem).replace(extension, "") for elem in image_paths
+    ]
 
     for image in images:
         process_docker_image(image, version, repo_name, build_args)
+
 
 def run(args):
     repo_name = get_repo_name()
@@ -90,10 +109,11 @@ def run(args):
 
     if len(args) > 0:
         image = args[0]
-        build_args = "".join(args[1:])
+        build_args = " ".join(args[1:])
         process_docker_image(image, version, repo_name, build_args)
     else:
         build_all_images("")
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
