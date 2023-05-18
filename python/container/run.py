c3f56638b5e262de10d58cfa3910cbbc8e2e535a
Julien Bongars <julien.bongars@airrlabs.com>
2023-05-18 06:53:52 +0000

    feat: temp containers

--- python/container/run.py
+++ python/container/run.py
@@ -1,8 +1,5 @@
-import requests
-import os
 import subprocess
 import sys
-import signal
 
 from .catalogue import get_available_containers
 from .utils import attach_git, attach_work, check_if_docker_image_exists, get_dtools_image_name, prune_flags
@@ -27,49 +24,6 @@ def get_image_and_start_script_from_function_name(function_name):
     return image_name, start_script
 
 
-def get_default_state_file_path():
-    return os.path.join(".", ".dtools", "state.tar")
-
-
-# def import_state_to_base_image(image_name, state_file_path):
-#     print("Importing state...")
-#     new_image = ""
-#     subprocess.run(
-#         f"docker import {state_file_path} {image_name}",
-#         shell=True,
-#         check=True,
-#         stdout=subprocess.PIPE,
-#     )
-
-#     return image_name
-
-# def run_container_with_state(function_name, state_file_path, args_string):
-#     image_name, start_script = get_image_and_start_script_from_function_name(
-#         function_name)
-
-#     state_parent_dir = os.path.dirname(state_file_path)
-#     if not os.path.isdir(state_parent_dir):
-#         os.makedirs(state_parent_dir)
-
-#     if os.path.isfile(state_file_path):
-#         image_name = import_state_to_base_image(image_name, state_file_path)
-
-#     command = f"docker create -ti {attach_work()} {attach_git()} {args_string} {image_name} {start_script}"
-
-#     container_id = subprocess.run(
-#         command,
-#         shell=True,
-#         check=True,
-#         stdout=subprocess.PIPE,
-#     ).stdout.decode("utf-8").strip()
-
-#     subprocess.run(f"docker start -ai {container_id}", shell=True, check=True)
-#     subprocess.run(f"docker export {container_id} > {state_file_path}",
-#                    shell=True,
-#                    check=True)
-#     subprocess.run(f"docker rm {container_id}", shell=True, check=True)
-
-
 def run_container(function_name, args_string):
     image_name, start_script = get_image_and_start_script_from_function_name(
         function_name)
@@ -92,6 +46,9 @@ def run(args):
 
     if function_name[0] == "." or function_name[0] == "/":
         return run_local_container(function_name, args_string)
+
+    if "_" == function_name[0]:
+        return run_temp_container(function_name.replace("_", ""), args_string)
 
     print(f"Function '{function_name}' not found. Using default...")
     try:
--- python/container/temp_container.py
+++ python/container/temp_container.py
@@ -1,21 +1,122 @@
+from distutils.dir_util import copy_tree
+import hashlib
 import os
+import shutil
 import subprocess
 
-from .utils import get_dtools_image_name
+from .utils import attach_git, attach_work, getcwd
 
 
-def get_template_base_path(os):
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(script_path, "../templates/modules/{os}/base.j2")
+def get_templates_path():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.abspath(os.path.join(current_path, "../../templates"))
 
 
-def get_base(os):
-    base_path = get_template_base_path(os)
+def get_template_base_path(image_os):
+    script_path = get_templates_path()
+    base_path = os.path.join(script_path, f"modules/{image_os}/base.j2")
+
+    print("base_path= ", base_path)
+
+    return base_path if os.path.exists(base_path) else None
+
+
+def extract_os_from_args(args_string):
+    if "--os-" in args_string:
+        return args_string.split("--os-")[1].split(
+            " ")[0], args_string.replace(f"--os-{os}", "")
+    return None, args_string
+
+
+def get_base(image_os):
+    if image_os is None:
+        return None
+
+    base_path = get_template_base_path(image_os)
+    if base_path is None:
+        return None
+
     with open(base_path) as f:
         return f.read()
 
 
-def run_temp_container():
-    base = get_base("ubuntu")
+def get_temp_folder_path():
+    if os.path.exists(os.environ["TEMP"]):
+        return os.environ["TEMP"]
+    if os.path.exists("/usr/tmp"):
+        return "/usr/tmp"
+    if os.path.exists("/tmp"):
+        return "/tmp"
+    return None
 
-    print(base)
+
+def create_temp_dockerfile(image, image_version, base):
+    temp_folder_path = get_temp_folder_path()
+    if temp_folder_path is None:
+        print("Could not find a temp folder. Creating...")
+        os.mkdir(os.path.join(getcwd(), ".dtemp"))
+        temp_folder_path = os.path.join(getcwd(), ".dtemp")
+
+    docker_folder_path = os.path.join(temp_folder_path, f"dtemp-{image}")
+    if not os.path.exists(docker_folder_path):
+        os.mkdir(docker_folder_path)
+
+    dockerfile_path = os.path.join(docker_folder_path, "Dockerfile")
+    with open(dockerfile_path, "w") as f:
+        f.write(f"FROM {image}:{image_version}\n")
+        f.write(base)
+        f.write("\n")
+        f.write("CMD [\"/bin/bash\"]")
+
+    copy_tree(f"{get_templates_path()}/scripts/",
+              f"{docker_folder_path}/scripts/")
+
+    return docker_folder_path
+
+
+def build_temp_image(image_name, image_version, dockerfile_path):
+    tag = f"dtemp-{image_name}:{image_version}"
+    command = f"docker build {dockerfile_path} -t {tag}"
+    print(f"Build command: {command}")
+
+    subprocess.run(command, shell=True)
+    return tag
+
+
+def get_image_name_version(image):
+    if len(image.split(":")) < 2:
+        return image, "latest"
+    return image.split(":")[0], image.split(":")[1]
+
+
+def run_temp_container(image, args_string):
+    image_os, args_string = extract_os_from_args(args_string)
+    image_name, image_version = get_image_name_version(image)
+
+    print(f"Running container <{image_name}:{image_version}>")
+
+    if image_os is None:
+        image_os = image_name
+
+    base = get_base(image_os)
+
+    if base is None:
+        print(f"Base template for {image_os} not found.")
+        return
+
+    dockerfile_path = create_temp_dockerfile(image_name, image_version, base)
+    print(f"Using Dockerfile {dockerfile_path}...")
+
+    tag = build_temp_image(image_name, image_version, dockerfile_path)
+    print(f"Built image {tag}...")
+
+    command = f"docker run -it {attach_git()} {attach_work()} --rm {args_string} {tag}"
+    print(command)
+
+    try:
+        subprocess.run(command, shell=True)
+    except:
+        print(f"Container '{image_name}' exited or could not be run.")
+
+    print('Cleaning up...')
+    # shutil.rmtree(os.path.dirname(dockerfile_path))
