bcdbc2425ed223219f4eb813f5cf1669d1fe3e1e
Julien Bongars <julien.bongars@airrlabs.com>
2023-07-07 11:39:14 +0800

    feat: added command description before running

--- python/container/custom_containers.py
+++ python/container/custom_containers.py
@@ -17,36 +17,35 @@ def attach_terraform_token():
     return f"-v {gethomedir()}/.terraform.d:/root/.terraform.d"
 
 
+def run_command(command):
+    print("command = ", command)
+    subprocess.run(command, shell=True, check=True)
+
+
 def ubuntu(args_string):
-    subprocess.run(
-        f"docker run -ti {attach_work()} {attach_git()} --rm {args_string} ubuntu:latest bash",
-        shell=True,
-        check=True)
+    run_command(
+        f"docker run -ti {attach_work()} {attach_git()} --rm {args_string} ubuntu:latest bash"
+    )
 
 
 def dubuntu(args_string):
-    subprocess.run(
-        f"docker run -ti {attach_work()} {attach_git()} --rm {args_string} dtools_ubuntu:latest zsh",
-        shell=True,
-        check=True)
+    run_command(
+        f"docker run -ti {attach_work()} {attach_git()} --rm {args_string} dtools_ubuntu:latest zsh"
+    )
 
 
 def aws(args_string):
-    subprocess.run(
-        f"docker run -it {attach_work()} {attach_git()} {attach_terraform_token()} -v {gethomedir()}/.aws:/root/.aws --rm {args_string} dtools_aws:latest zsh",
-        shell=True,
-        check=True)
+    run_command(
+        f"docker run -it {attach_work()} {attach_git()} {attach_terraform_token()} -v {gethomedir()}/.aws:/root/.aws --rm {args_string} dtools_aws:latest zsh"
+    )
 
 
 def ngrok(args_string):
-    subprocess.run(
-        f"docker run --network host --rm {args_string} dtools_ngrok:latest",
-        shell=True,
-        check=True)
+    run_command(
+        f"docker run --network host --rm {args_string} dtools_ngrok:latest")
 
 
 def github_actions(args_string):
     image_name = get_dtools_image_name("github_actions")
-    subprocess.run(f"docker run -ti --rm {args_string} {image_name}",
-                   shell=True,
-                   check=True)
+    command = f"docker run -ti --rm {args_string} {image_name}"
+    run_command(command)
