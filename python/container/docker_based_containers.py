ac8927d3c309762b5baa62241e82c3b9f9c86cf0
Julien Bongars <julien.bongars@airrlabs.com>
2023-12-04 12:19:14 +0800

    fix: docker containers

--- python/container/docker_based_containers.py
+++ python/container/docker_based_containers.py
@@ -5,13 +5,13 @@ import subprocess
 
 from .utils import get_dtools_image_name, attach_git, attach_work, set_hostname
 
+DEFAULT_EXEC_STRING = "/bin/sh"
+DOOD_FLAG = "--dood"
+DIND_FLAG = "--dind"
+
 
 def availalbe_containers():
-    return {
-        "kube": kube,
-        "awskube": awskube,
-        "ddocker":  ddocker
-    }
+    return {"kube": kube, "awskube": awskube, "ddocker": ddocker}
 
 
 def stop_container(container_id):
@@ -32,70 +32,75 @@ def safely_exec_container(container_id, exec_string):
     for sig in (signal.SIGTERM, signal.SIGINT, signal.SIGABRT):
         signal.signal(sig, cleanup_handler)
 
-    subprocess.run(f"docker exec -it {container_id} {exec_string}",
-                   shell=True,
-                   check=True)
+    subprocess.run(
+        f"docker exec -it {container_id} {exec_string}", shell=True, check=True
+    )
     stop_container(container_id)
 
     cleanup_flag = False
 
 
-def is_dood():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dood', action='store_true', help='enable dood mode')
-    args, unknown = parser.parse_known_args()
-    return args.dood, unknown
-
-def run_dood_container(image_name, base_command, args_string, exec_dood_string="zsh"):
+def run_dood_container(
+    image_name, base_command, args_string, exec_string=DEFAULT_EXEC_STRING
+):
     hostname = f"dtools-{image_name}-dood"
-    cmd = f"{base_command} {set_hostname(hostname)} -v /var/run/docker.sock:/var/run/docker.sock {args_string} {image_name} {exec_dood_string}"
-    return  subprocess.run(cmd, shell=True, check=True)
+    cmd = f"{base_command} {set_hostname(hostname)} -v /var/run/docker.sock:/var/run/docker.sock {args_string} {image_name} {exec_string}"
+    return subprocess.run(cmd, shell=True, check=True)
 
-def run_dind_container(image_name, base_command, args_string, exec_dind_string="zsh"):
-    hostname = f"dtools-{image_name}-dood"
+
+def run_dind_container(
+    image_name, base_command, args_string, exec_dind_string=DEFAULT_EXEC_STRING
+):
+    hostname = f"dtools-{image_name}-dind"
 
     # privileged is required for docker in docker
     # docs: https://hub.docker.com/_/docker > Rootless
     cmd = f"{base_command} {set_hostname(hostname)} --privileged {args_string} {image_name}"
 
     subprocess.run(cmd, shell=True, check=True)
-    container_id = subprocess.check_output(
-        f"docker ps -q -f ancestor={image_name}",
-        shell=True).decode().strip().split("\n")[0]
-
+    container_id = (
+        subprocess.check_output(f"docker ps -q -f ancestor={image_name}", shell=True)
+        .decode()
+        .strip()
+        .split("\n")[0]
+    )
     safely_exec_container(container_id, exec_dind_string)
 
 
-def run_docker_container(image_name,
-                         base_command,
-                         args_string,
-                         exec_dind_string="zsh"):
+def run_docker_container(
+    image_name, dood_command, dind_command, args_string, exec_dind_string="fish"
+):
+    print(args_string)
+    if DOOD_FLAG in args_string:
+        print("Running with dood flag...")
+        args_string = args_string.replace(DOOD_FLAG, "")
+        return run_dood_container(image_name, dood_command, args_string)
 
-    if "--dood " in args_string:
-        args_string = args_string.replace("--dood ", "")
-        return run_dood_container(image_name, base_command, args_string, exec_dind_string)
-    
     else:
-        if "--dind " in args_string:
-            args_string = args_string.replace("--dind ", "")
-        return run_dind_container(image_name, base_command, args_string, exec_dind_string)
+        print("Running with dind flag...")
+        if DIND_FLAG in args_string:
+            args_string = args_string.replace(DIND_FLAG, "")
+        return run_dind_container(
+            image_name, dind_command, args_string, exec_dind_string
+        )
+
 
 def ddocker(args_string):
     image_name = get_dtools_image_name("docker")
-    dood_command = f"docker run -ti {attach_work()}  {set_hostname('dtools-docker-dood')} {attach_git()} --rm -v /var/run/docker.sock:/var/run/docker.sock {args_string} {image_name} zsh"
-    dind_command = f"docker run -d {attach_work()}  {set_hostname('dtools-docker-dind')} {attach_git()} --privileged {args_string} {image_name}"
-    run_docker_container(image_name, dood_command, dind_command)
+    dood_command = f"docker run -ti {attach_work()} {attach_git()} --rm"
+    dind_command = f"docker run -d {attach_work()} {attach_git()}"
+    run_docker_container(image_name, dood_command, dind_command, args_string, "fish")
 
 
 def kube(args_string):
     image_name = get_dtools_image_name("kube")
-    dood_command = f"docker run -ti {attach_work()}  {set_hostname('dtools-kube-dood')} {attach_git()} --rm -v /var/run/docker.sock:/var/run/docker.sock {args_string} {image_name} zsh"
-    dind_command = f"docker run -d {attach_work()}  {set_hostname('dtools-kube-dind')} {attach_git()} --privileged {args_string} {image_name}"
-    run_docker_container(image_name, dood_command, dind_command)
+    dood_command = f"docker run -ti {attach_work()} {attach_git()} --rm"
+    dind_command = f"docker run -d {attach_work()} {attach_git()}"
+    run_docker_container(image_name, dood_command, dind_command, args_string, "fish")
 
 
 def awskube(args_string):
     image_name = get_dtools_image_name("awskube")
-    dood_command = f"docker run -it {attach_work()}  {set_hostname('dtools-awskube-dood')} {attach_git()} -v {os.path.expanduser('~')}/.aws:/root/.aws -v /var/run/docker.sock:/var/run/docker.sock --rm {args_string} {image_name} zsh"
-    dind_command = f"docker run -d {attach_work()}  {set_hostname('dtools-awskube-dind')} {attach_git()} -v {os.path.expanduser('~')}/.aws:/root/.aws --privileged {args_string} {image_name}"
-    run_docker_container(image_name, dood_command, dind_command)
+    dood_command = f"docker run -it {attach_work()} {attach_git()} -v {os.path.expanduser('~')}/.aws:/root/.aws --rm"
+    dind_command = f"docker run -d {attach_work()} {attach_git()} -v {os.path.expanduser('~')}/.aws:/root/.aws"
+    run_docker_container(image_name, dood_command, dind_command, args_string, "fish")
